e1f5a4cc20483b74bcfbb0e252a69661188f4a09
DESKTOP-57EJFHI\LeeBeomJun <qjawnswkd0717@gmail.com>
2021-02-13 13:57:59 +0900

    피보나치 DP

--- src/com/company/recursion/Fibonacci.java
+++ src/com/company/recursion/Fibonacci.java
@@ -6,24 +6,29 @@ import java.util.Scanner;
 
 public class Fibonacci {
 
-    static int data[] = new int[200];
+    static long data[] = new long[201];
 
     //피보나치 수열
     public static int fibonacci(int n) {
         if(n == 1 || n == 2) return 1;
         if(n == 3) return 2;
-        data[n] =  fibonacci(n - 1) + fibonacci(n - 2);
-        return data[n];
+        return fibonacci(n - 1) + fibonacci(n - 2);
+
     }
+
+    //피보나치 메모이제이션
+    public static long fibonacciArr(int n) {
+        if(data[n] != 0) return data[n];
+        if(n == 1 || n == 2) return data[n] = 1;
+        return data[n] = fibonacciArr(n - 1) + fibonacciArr(n - 2)  % 10009;
+    }
+
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n;
         System.out.print("입력하세요: ");
         n = scan.nextInt();
-        System.out.println(n+"번째의 수는 : " + fibonacci(n) % 10009);
-        Arrays.stream(data).iterator();
-
-
+        System.out.println(n+"번째의 수는 : " + fibonacciArr(n) % 10009);
     }
 }
