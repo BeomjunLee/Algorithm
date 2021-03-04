91d88e447cecda524f05a010f5cda6d51b4b317d
DESKTOP-57EJFHI\LeeBeomJun <qjawnswkd0717@gmail.com>
2021-03-04 23:00:00 +0900

    recursion 연습

--- src/com/company/recursion/HailCount.java
+++ src/com/company/recursion/HailCount.java
@@ -1,6 +1,7 @@
 package com.company.recursion;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
@@ -61,16 +62,63 @@ public class HailCount {
         printHailCountReverse(n - 1);   //n을 1씩 줄여서 재귀
     }
 
+//    static HashMap<Integer, Integer> hash = new HashMap<>();
+//    static int count = 0;
+//    /**
+//     * 우박 수 길이 (3n + 1) Large
+//     */
+//    public static void hailCountLarge(int a, int b) {
+//        if(a == 1) arr[a] = 1;
+//
+//        data.add(a);
+//        if(data.get(0) > b) return;
+//
+//        if(arr[a] != 0) {
+//            ++count;
+//        }
+//        else{
+//            if(a % 2 == 1) arr[a] = 3 * a + 1;
+//            else arr[a] = a / 2;
+//            ++count;
+//            hailCountLarge(arr[a], b);
+//        }
+//        if(a == 1) {
+//            int num = data.get(0);  //List 0번째 값 빼오기
+//            data.clear();           //List 초기화
+//
+//            hash.put(num, count);   //HashMap에 값 넣기
+//
+//            count = 0;              //count 초기화
+//            hailCountLarge(num + 1, b);
+//        }
+//    }
+//
+//    static int arr[] = new int[10000001];
+//
+//    public static int hailCountLarge2(int a) {
+//        if(a == 1) return 1;
+//        if(arr[a] != 0) return arr[a];
+//        else{
+//            if(a % 2 == 1) return arr[a] = hailCountLarge2(3 * a + 1);
+//            else return arr[a] = hailCountLarge2(a / 2);
+//        }
+//    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int n;
+        int n, b;
         System.out.print("입력하세요: ");
         n = scan.nextInt();
-        hailCount(n);
-        //////////////////
-        System.out.println("==================");
-        hailCountReverse(n);
-        printHailCountReverse(data.size());
+        b = scan.nextInt();
+//        hailCount(n);
+//        //////////////////
+//        System.out.println("==================");
+//        hailCountReverse(n);
+//        printHailCountReverse(data.size());
+//        System.out.println("==================");
+//        hailCountLarge(n, b);
+//        hash.forEach((key, value) -> System.out.println(key + "|" + value));
+//        System.out.println(hailCountLarge2(n));
     }
 
 }
